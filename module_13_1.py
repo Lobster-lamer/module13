8c5e9aac14d50f1f029cd1ca824a4c0a83e5da65
Lobster-lamer <101044580+Lobster-lamer@users.noreply.github.com>
2024-11-27 21:56:41 +0300

    Add files via upload

--- module_13_1.py
+++ module_13_1.py
@@ -7,23 +7,23 @@ BALLS_COUNT = 5
 async def start_strongman(name: str, power: int):
     if 0 < power < 10:
         global BALLS_COUNT
-        print(f"Качок {CoTeSt.colorful_str(name, CoTeSt.Color.CYAN)} начал соревнования")
-        time_to_lift_up_the_ball = 10 - power
+        print(f"Силач {CoTeSt.colorful_str(name, CoTeSt.Color.CYAN)} начал соревнования")
+        time_to_lift_up_the_ball = 10 / power
         for ball_number in range(1, BALLS_COUNT+1):
             await asyncio.sleep(time_to_lift_up_the_ball)
-            print(f"Качок {CoTeSt.colorful_str(name, CoTeSt.Color.CYAN)} поднял шар №"
-                  f"{CoTeSt.colorful_str(str(ball_number), CoTeSt.Color.BLUE)}")
+            print(f"Силач {CoTeSt.colorful_str(name, CoTeSt.Color.CYAN)} поднял "
+                  f"{CoTeSt.colorful_str(str(ball_number), CoTeSt.Color.BLUE)} шар")
         print(f"{CoTeSt.colorful_str(name, CoTeSt.Color.CYAN)} "
               f"{CoTeSt.Color.GREEN}закончил соревнования{CoTeSt.REGULAR}")
 
 
 async def start_tournament():
-    first_jock = asyncio.create_task(start_strongman("Man", 9))
-    second_jock = asyncio.create_task(start_strongman("M'eh", 7))
-    third_jock = asyncio.create_task(start_strongman("Dude", 8))
-    await first_jock
-    await second_jock
-    await third_jock
+    first_strongman = asyncio.create_task(start_strongman("Appolon", 5))
+    second_strongman = asyncio.create_task(start_strongman("Denis", 4))
+    third_strongman = asyncio.create_task(start_strongman("Pasha", 3))
+    await first_strongman
+    await second_strongman
+    await third_strongman
 
 
 asyncio.run(start_tournament())
